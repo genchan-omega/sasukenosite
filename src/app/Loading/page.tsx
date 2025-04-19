365b52a4824fd760a5a68418f9798fc9d3c42c22
genchan_omega <106954010+genchan-omega@users.noreply.github.com>
2025-04-20 05:34:14 +0900

    modify a bit

---
+++ src/app/Loading/page.tsx
@@ -0,0 +1,16 @@
+// /src/app/Profile/page.tsx
+
+"use client";
+
+import classes from "./globals.module.css"
+
+export default function Loading() {
+
+  return (
+    <main className="fixed top-2 right-2 -translate-2">
+      <div
+        className={classes.box}>
+      </div>
+    </main>
+  );
+}
---
+++ src/app/loading.tsx
@@ -0,0 +1,19 @@
+// /src/loading.tsx
+
+import "./globals.css";
+
+export default function Loading() {
+  return (
+    <main className="h-screen w-screen">
+      <div className={`animation-box`}>
+
+      </div>
+      
+
+
+
+
+
+    </main>
+  );
+}
